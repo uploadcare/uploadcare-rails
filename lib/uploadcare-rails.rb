27c2efdef65da2abb3f2cd7534999d052ec77c88
Alex Zinchenko <admloki@gmail.com>
2012-07-07 02:01:44 +1100

    0.1.0 finally ready

--- lib/uploadcare-rails.rb
+++ lib/uploadcare-rails.rb
@@ -32,88 +32,66 @@ module Uploadcare
     module ClassMethods
       def has_uploadcare_file(name, options = {})
         include InstanceMethods
+
+        options[:file_column] ||= name.to_s
+        options[:auto_keep] = true if options[:auto_keep].nil?
         
-        options[:file_column]      ||= "uploadcare_file"
-        #options[:file_info_column] ||= "uploadcare_file_info"
-        options[:auto_keep]        ||= true
-        
-        serialize :"#{options[:file_info_column]}", Hash
-        
-        #unless self.column_names.include?(options[:uuid_column])
-        #  raise DatabaseError, "UUID column not found in columns list, please run 'uploadcare_rails' generator to generate migration and 'rake db:migrate' to apply changes to database.", caller
-        #end
+        unless self.column_names.include?(options[:file_column])
+          raise DatabaseError, "File column not found in columns list, please generate a migration to create a file column and 'rake db:migrate' to apply changes to database.", caller
+        end
         
         define_method name do
           upload_for name, options
         end
         
         define_method "#{name.to_s}_before_type_cast" do
-          self.instance_variable_get("@_#{name.to_s}")
-        end
-        
-        define_method "#{name.to_s}?" do
-          upload_for(name, options).exist?
+          read_attribute(name.to_sym)
         end
         
         define_method "#{name.to_s}=" do |uuid|
-          #unless uuid.blank?
-            self.instance_variable_set("@_#{options[:uuid_column]}", read_attribute(options[:uuid_column]))
-            self.instance_variable_set("@_#{name.to_s}", uuid)
-            upload_for(name, options).assign_uuid(uuid)
-          #end
-        end
-        
-        define_method "#{name.to_s}_modified?" do
-          modified   = false
-          check_uuid = self.instance_variable_get("@_#{name.to_s}")
-          
-          if new_record?
-            modified = !check_uuid.blank?
-          else
-            old_uuid = self.instance_variable_get("@_#{options[:uuid_column]}")
-            modified = !check_uuid.blank? && (old_uuid.blank? || old_uuid != check_uuid)
-          end
-          modified
-        end
-        
-        define_method "reload_file_info" do
-          upload_for(name, options).reload
+          self.instance_variable_set("@_#{options[:file_column]}", read_attribute(options[:file_column]))
+          self.instance_variable_set("@_#{name.to_s}", uuid)
+          write_attribute(name.to_sym, uuid)
+          upload_for(name, options).assign_uuid(uuid)
         end
       end
+
+    # TODO: Think about validation
+    #      
+    #   def validates_upload_type(name, options = {})
+    #   end
       
-      def validates_upload_type(name, options = {})
-      end
-      
-      def validates_upload_size(name, options = {})
-        min     = options[:min] || options[:in] && options[:in].first || 0
-        max     = options[:max] || options[:in] && options[:in].last || 0
-        message = options[:message] || "file size must be between :min and :max bytes"
-        message = message.gsub(/:min/, min.to_s).gsub(/:max/, max.to_s)
+    #   def validates_upload_size(name, options = {})
+    #     min     = options[:min] || options[:in] && options[:in].first || 0
+    #     max     = options[:max] || options[:in] && options[:in].last || 0
+    #     message = options[:message] || "file size must be between :min and :max bytes"
+    #     message = message.gsub(/:min/, min.to_s).gsub(/:max/, max.to_s)
         
-        validates_each :"#{name}" do |record, attr, value|
-          if_clause_passed = options[:if].nil? || (options[:if].respond_to?(:call) ? options[:if].call(record) != false : record.send(options[:if]))
-          unless_clause_passed = options[:unless].nil? || (options[:unless].respond_to?(:call) ? !!options[:unless].call(record) == false : !record.send(options[:unless]))
-          upload = record.send("#{name}")
-          if upload.info_loaded?
-            if if_clause_passed && unless_clause_passed && (upload.size < min || (upload.size > max && !max.zero?))
-              record.errors.add("#{name}", message)
-            end
-          else
-            record.errors.add("#{name}", message)
-          end
-        end
-      end
+    #     validates_each :"#{name}" do |record, attr, value|
+    #       if_clause_passed = options[:if].nil? || (options[:if].respond_to?(:call) ? options[:if].call(record) != false : record.send(options[:if]))
+    #       unless_clause_passed = options[:unless].nil? || (options[:unless].respond_to?(:call) ? !!options[:unless].call(record) == false : !record.send(options[:unless]))
+    #       upload = record.send("#{name}")
+    #       if upload.info_loaded?
+    #         if if_clause_passed && unless_clause_passed && (upload.size < min || (upload.size > max && !max.zero?))
+    #           record.errors.add("#{name}", message)
+    #         end
+    #       else
+    #         record.errors.add("#{name}", message)
+    #       end
+    #     end
+    #   end
       
-      def validates_upload_presence(name, options = {})
-        message = options[:message] || "must be present"
-        validates_each :"#{name}" do |record, attr, value|
-          if_clause_passed = options[:if].nil? || (options[:if].respond_to?(:call) ? options[:if].call(record) != false : record.send(options[:if]))
-          unless_clause_passed = options[:unless].nil? || (options[:unless].respond_to?(:call) ? !!options[:unless].call(record) == false : !record.send(options[:unless]))
-          if if_clause_passed && unless_clause_passed && !value.valid_upload?
-            record.errors.add("#{name}", message)
-          end
-        end
-      end
+    #   def validates_upload_presence(name, options = {})
+    #     message = options[:message] || "must be present"
+    #     validates_each :"#{name}" do |record, attr, value|
+    #       if_clause_passed = options[:if].nil? || (options[:if].respond_to?(:call) ? options[:if].call(record) != false : record.send(options[:if]))
+    #       unless_clause_passed = options[:unless].nil? || (options[:unless].respond_to?(:call) ? !!options[:unless].call(record) == false : !record.send(options[:unless]))
+    #       if if_clause_passed && unless_clause_passed && !value.valid_upload?
+    #         record.errors.add("#{name}", message)
+    #       end
+    #     end
+    #   end
+    
     end
     
     module InstanceMethods
--- lib/uploadcare-rails/upload.rb
+++ lib/uploadcare-rails/upload.rb
@@ -9,59 +9,49 @@ class Uploadcare::Rails::Upload
     @_field_method = field_method
     @_options = options
     @_instance = instance
-    @_file_info = Hash.new
     @uuid = self.uuid_value
-    self.load_file_info(false)
-  end
-  
-  def exist?
-    !@uuid.blank? && info_loaded? && !removed?
   end
   
   def removed?
-    !@_file_info["removed"].blank? && @_file_info["removed"]
+    inf = self.file_info
+    !inf["removed"].blank? && inf["removed"]
   end
   
   def image?
     IMAGE_MIMES.include?(@_file_info["mime_type"])
   end
   
-  def valid_upload?
-    exist?
-  end
-  
-  def info_loaded?
-    !@_file_info.blank?
-  end
-  
   def url
-    @_file_info["url"]
+    self.file_info["url"]
   end
   
   def resized_url(to = '640x480')
     url = [SERVICES_URL].push('resizer').push(@uuid).push(to).join('/')
-    url
+    url + "/"
   end
   
   def crop_url(to = '640x480')
     url = [self.resized_url(to)].push('crop').join('/')
-    url
+    url + "/"
   end
   
   def original_url
-    @_file_info["original_file_url"]
+    self.file_info["original_file_url"]
   end
   
   def size
-    @_file_info["size"].blank? ? 0 : @_file_info["size"]
+    self.file_info["size"].blank? ? 0 : self.file_info["size"]
   end
   
-  def keep?
-    !@_file_info["last_keep_claim"].blank?
+  def kept?
+    !self.file_info["last_keep_claim"].blank?
+  end
+
+  def file_info
+    get_file_info
   end
   
   def assign_uuid(new_uuid)
-    @_instance.send("#{self.uuid_column_name}=", new_uuid) if @_instance.respond_to?(self.uuid_column_name)
     if @_options[:auto_keep]
       self.keep 
     else
@@ -70,39 +60,21 @@ class Uploadcare::Rails::Upload
   end
   
   def uuid_value
-    (@_instance.new_record? || @_instance.send(self.uuid_column_name).blank?) ? @_instance.send("#{@_field_method.to_s}_before_type_cast") : @_instance.send(self.uuid_column_name)
+    @_instance.send("#{@_field_method.to_s}_before_type_cast")
   end
 
-  def uuid_column_name
-    @_options[:uuid_column]
-  end
-  
   def keep
     f = self.get_file_instance
     f.keep
     self.reload
   end
   
-  # TODO: Direct saving to model
-  def load_file_info(remote_fetch = true)
-    @_file_info = @_instance.send("#{@_options[:file_info_column]}")
-    
-    if remote_fetch && @_file_info.blank?
-      @_file_info = self.get_file_info
-      @_instance.send("#{@_options[:file_info_column]}=", @_file_info)
-    end
-  end
-  
   def reload
-    @_file_info = self.get_file_info
-    @_instance.send("#{@_options[:file_info_column]}=", @_file_info)
     save_instance
   end
   
   def remove
     self.get_file_instance.remove
-    @_file_info["removed"] = true
-    @_instance.send("#{@_options[:file_info_column]}=", @_file_info)
     save_instance
   end
   
