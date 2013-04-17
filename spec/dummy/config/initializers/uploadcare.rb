71ff805a4c165bd2195b230e244feab84875642e
Dimitry Solovyov <dimituri@gmail.com>
2013-04-17 14:24:55 +0300

    Default bases in dummy app

--- spec/dummy/config/initializers/uploadcare.rb
+++ spec/dummy/config/initializers/uploadcare.rb
@@ -1,7 +1,5 @@
 Uploadcare::Rails::Engine.configure do
-  config.uploadcare.upload_url_base = 'https://upload.uploadcare.com'
-  config.uploadcare.api_url_base = 'https://api.uploadcare.com'
-  config.uploadcare.static_url_base = 'https://ucarecdn.com'
   config.uploadcare.public_key = 'demopublickey'
   config.uploadcare.private_key = 'demoprivatekey'
+  config.uploadcare.widget_version = '0.7.0.1'
 end
