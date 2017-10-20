691573d53d534d869841d5e7bd631e0a333ad79d
vizvamitra <vizvamitra@gmail.com>
2017-10-20 13:57:28 +0300

    Fix error when running `rails generate uploadcare_config` without a config/uploadcare.yml file
    - convert config to a hash with indifferent access in Uploadcare::Rails::Settings initializer
    - test Uploadcare::Api::Settings and config generator

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,28 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140320092807) do
+ActiveRecord::Schema.define(version: 20171012001801) do
 
   create_table "post_with_collections", force: :cascade do |t|
-    t.string   "title"
-    t.string   "file"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string "title"
+    t.string "file"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "posts", force: :cascade do |t|
-    t.string   "title"
-    t.string   "file"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string "title"
+    t.string "file"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "posts_with_collection_and_files", force: :cascade do |t|
-    t.string   "title"
-    t.string   "file"
-    t.string   "group"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string "title"
+    t.string "file"
+    t.string "group"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
