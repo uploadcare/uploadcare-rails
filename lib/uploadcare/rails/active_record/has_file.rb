79f628f5d1b713f07808c9f98c5897446bf5d5ef
Dale Campbell <oshuma@gmail.com>
2018-01-26 15:26:52 -0500

    Use attribute when defining #build_file and #build_group methods.
    Issue uploadcare/uploadcare-rails#61

--- lib/uploadcare/rails/active_record/has_file.rb
+++ lib/uploadcare/rails/active_record/has_file.rb
@@ -13,7 +13,7 @@ module Uploadcare
           false
         end
 
-        define_method 'build_file' do
+        define_method "build_#{attribute}_file" do
           cdn_url = attributes[attribute.to_s].to_s
           return nil if cdn_url.empty?
 
@@ -32,7 +32,7 @@ module Uploadcare
         # it has some helpers for rails enviroment
         # but it also has all the methods of Uploadcare::File so no worries.
         define_method "#{ attribute }" do
-          build_file
+          send("build_#{attribute}_file".to_sym)
         end
 
         define_method "check_#{ attribute }_for_uuid" do
@@ -44,7 +44,7 @@ module Uploadcare
         end
 
         define_method "store_#{ attribute }" do
-          file = build_file
+          file = send("build_#{attribute}_file".to_sym)
 
           return unless file
 
@@ -60,7 +60,7 @@ module Uploadcare
         end
 
         define_method "delete_#{ attribute }" do
-          file = build_file
+          file = send("build_#{attribute}_file".to_sym)
 
           begin
             file.delete
