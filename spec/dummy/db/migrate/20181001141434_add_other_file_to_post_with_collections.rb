2829a3754253083af8c9bd1773c5bdd7903cd8a7
Vadim Rastyagaev <vadim.rastyagaev@toptal.com>
2018-10-01 18:07:37 +0200

    Fix migration

--- spec/dummy/db/migrate/20181001141434_add_other_file_to_post_with_collections.rb
+++ spec/dummy/db/migrate/20181001141434_add_other_file_to_post_with_collections.rb
@@ -1,4 +1,7 @@
-class AddOtherFileToPostWithCollections < ActiveRecord::Migration[5.2]
+superclass = ActiveRecord::Migration
+superclass = ActiveRecord::Migration[5.0] if superclass.respond_to?(:[])
+
+class AddOtherFileToPostWithCollections < superclass
   def change
     add_column :post_with_collections, :other_file, :string
   end
