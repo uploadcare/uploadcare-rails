922dc22b0c8124921725d7ee557994b3cf25d909
dmitrij <dmitrij.ivanchenko@gmail.com>
2021-07-20 13:27:49 +0600

    Initial commit

---
+++ lib/uploadcare/rails.rb
@@ -0,0 +1,8 @@
+require "uploadcare/rails/version"
+
+module Uploadcare
+  module Rails
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/uploadcare/rails_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Uploadcare::Rails do
+  it "has a version number" do
+    expect(Uploadcare::Rails::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
