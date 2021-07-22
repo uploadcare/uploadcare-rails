9b752a5552c0ecc6e00f36bdd2091de7fd1a2120
dmitrij <dmitrij.ivanchenko@gmail.com>
2021-07-22 15:22:57 +0600

    Rerecord vcr cassets for GroupApi specs

--- spec/uploadcare/rails/api/group_api_spec.rb
+++ spec/uploadcare/rails/api/group_api_spec.rb
@@ -11,7 +11,7 @@ module Uploadcare
 
         context 'when checking methods' do
           it 'responds to expected REST methods' do
-            %i[get_groups get_group].each do |method|
+            %i[get_groups get_group store_group].each do |method|
               expect(subject).to respond_to(method)
             end
           end
@@ -20,7 +20,7 @@ module Uploadcare
         context 'when sending requests' do
           it 'gets group info' do
             VCR.use_cassette('group_api_get_group') do
-              uuid = '38483b4e-57fe-40e7-8e52-74fbdd73da14~2'
+              uuid = '8b1362ed-b477-4a15-819a-2c6bb497d8bd~3'
               response = subject.get_group(uuid)
               expect(response['id']).to eq(uuid)
             end
@@ -43,38 +43,6 @@ module Uploadcare
               expect(response['id']).to eq(uuid)
             end
           end
-          #
-          # it 'stores a file' do
-          #   VCR.use_cassette('file_api_store_file') do
-          #     uuid = '5ae54c37-754c-4982-8de4-3f242a88ce17'
-          #     response = subject.store_file(uuid)
-          #     expect(response['uuid']).to eq uuid
-          #   end
-          # end
-          #
-          # it 'deletes a file' do
-          #   VCR.use_cassette('file_api_delete_file') do
-          #     uuid = '5ae54c37-754c-4982-8de4-3f242a88ce17'
-          #     response = subject.delete_file(uuid)
-          #     expect(response['uuid']).to eq uuid
-          #   end
-          # end
-          #
-          # it 'stores a batch of files' do
-          #   VCR.use_cassette('file_api_store_files') do
-          #     uuid = '64215d18-1356-42cb-ab8c-7542290b6e1b'
-          #     response = subject.store_files([uuid])
-          #     expect(response['result'].first['uuid']).to eq uuid
-          #   end
-          # end
-          #
-          # it 'stores a batch of files' do
-          #   VCR.use_cassette('file_api_delete_files') do
-          #     uuid = '37d70281-cc30-4c59-b8d6-e11c472dec40'
-          #     response = subject.delete_files([uuid])
-          #     expect(response['result'].first['uuid']).to eq uuid
-          #   end
-          # end
         end
       end
     end
