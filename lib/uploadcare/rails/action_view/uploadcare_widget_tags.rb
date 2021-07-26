5b7bf360d39d8c7612468b0db109703c880fabc5
dmitrij <dmitrij.ivanchenko@gmail.com>
2021-07-26 13:13:21 +0600

    Add helpers specs

--- lib/uploadcare/rails/action_view/uploadcare_widget_tags.rb
+++ lib/uploadcare/rails/action_view/uploadcare_widget_tags.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'action_view'
 require 'uploadcare/rails/configuration'
 
 module Uploadcare
@@ -21,9 +22,10 @@ module Uploadcare
         # Arguments:
         #   version: (String, default: '3.x') - version of the widget
         #   bundle: (String, default: 'full') - https://uploadcare.com/docs/uploads/file-uploader/#bundles
+        #     valid options: 'full', 'default', 'ie8', 'api', 'lang.en'
         #   min: (true/false, default: true) - sets which version to get, minified or not
 
-        UC_ORIGIN = 'ucarecdn.com'
+        UC_ORIGIN = ENV.fetch('UPLOADCARE_ORIGIN', 'ucarecdn.com')
 
         def uploadcare_widget_tag(version: '3.x', bundle: 'full', min: true)
           min = min == true ? '.min' : ''
