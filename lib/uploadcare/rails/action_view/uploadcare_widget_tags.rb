837efe12d4f4c43dedef2f71ad7b80c18aed56f1
Dmitrij Ivanchenko <dmitrij.ivanchenko@evrone.com>
2021-08-31 12:53:23 +0600

    Rename widget tag to include tag, divide global params into widget and common params

--- lib/uploadcare/rails/action_view/uploadcare_widget_tags.rb
+++ lib/uploadcare/rails/action_view/uploadcare_widget_tags.rb
@@ -6,7 +6,7 @@ module Uploadcare::Rails::ActionView
     # See https://uploadcare.com/docs/uploads/file-uploader/#cdn for more info.
     #
     # Example:
-    #   <%= uploadcare_widget_tag %>
+    #   <%= uploadcare_include_tag %>
     #   => <script src="https://ucarecdn.com/libs/widget/3.x/uploadcare.full.min.js"></script>
     #      <script>
     #        UPLOADCARE_PUBLIC_KEY = 'demopublickey';
@@ -18,15 +18,15 @@ module Uploadcare::Rails::ActionView
     #   bundle: (String, default: 'full') - https://uploadcare.com/docs/uploads/file-uploader/#bundles
     #   min: (true/false, default: true) - sets which version to get, minified or not
 
-    UC_ORIGIN = 'ucarecdn.com'
+    CDN_HOSTNAME = 'ucarecdn.com'
 
-    def uploadcare_widget_tag(version: '3.x', bundle: 'full', min: true)
+    def uploadcare_include_tag(version: '3.x', bundle: 'full', min: true)
       min = min == true ? '.min' : ''
       bundle = bundle == 'default' ? '' : ".#{bundle}"
-      path = ["/libs/widget/#{version}/", "uploadcare#{bundle}#{min}.js"].join
-      uri = URI::HTTPS.build(host: UC_ORIGIN, path: path)
+      path = "/libs/widget/#{version}/uploadcare#{bundle}#{min}.js"
+      uri = URI::HTTPS.build(host: CDN_HOSTNAME, path: path)
 
-      config_tag = javascript_tag(widget_settings) if widget_settings.present?
+      config_tag = javascript_tag(uploader_settings) if uploader_settings.present?
       include_tag = javascript_include_tag(uri.to_s.squeeze('.'))
 
       include_tag.concat(config_tag)
@@ -34,8 +34,8 @@ module Uploadcare::Rails::ActionView
 
     private
 
-    def widget_settings
-      @widget_settings ||= Uploadcare::Rails.configuration.widget_parameters
+    def uploader_settings
+      @uploader_settings ||= Uploadcare::Rails.configuration.uploader_parameters
     end
   end
 end
--- lib/uploadcare/rails/configuration.rb
+++ lib/uploadcare/rails/configuration.rb
@@ -5,23 +5,30 @@ module Uploadcare
     class Configuration
       include Singleton
 
-      CONFIG_GLOBAL_PARAMS = %w[
-        public_key locale live manual_start images_only preview_step crop image_shrink
+      PARAM_NAME_MAP = {
+        'signature_lifetime' => 'secure_expire'
+      }.freeze
+      WIDGET_PARAMS = %w[
+        public_key images_only preview_step crop image_shrink
         clearable tabs input_accept_types preferred_types system_dialog multipart_min_size
-        locale locale_translations locale_pluralize secure_signature secure_expire preview_proxy
-        preview_url_callback cdn_base do_not_store audio_bits_per_second video_preferred_mime_types
+        signature_lifetime preview_proxy
+        cdn_base do_not_store audio_bits_per_second video_preferred_mime_types
         video_bits_per_second camera_mirror_default
       ]
+      CONFIG_GLOBAL_PARAMS = %w[
+        locale locale_translations locale_pluralize live manual_start
+      ]
 
-      attr_accessor *CONFIG_GLOBAL_PARAMS
+      attr_accessor *(CONFIG_GLOBAL_PARAMS + WIDGET_PARAMS)
 
-      def widget_parameters
-        CONFIG_GLOBAL_PARAMS.map do |param_name|
-          param_value = instance_variable_get("@#{param_name}")
+      def uploader_parameters
+        (CONFIG_GLOBAL_PARAMS + WIDGET_PARAMS).map do |param_name|
+          mapped_name = PARAM_NAME_MAP[param_name] || param_name
+          param_value = instance_variable_get("@#{mapped_name}")
           next if param_value.nil?
 
           param_value = handle_param_value(param_value)
-          "UPLOADCARE_#{param_name.upcase} = #{param_value};"
+          "UPLOADCARE_#{mapped_name.upcase} = #{param_value};"
         end.compact.join("\n")
       end
 
@@ -30,7 +37,11 @@ module Uploadcare
       def handle_param_value(param_value)
         case param_value
         when Hash
-          param_value.deep_stringify_keys.to_s.tr('=>', ': ')
+          param_value.deep_stringify_keys.to_json
+        when Array
+          "'#{param_value.join(' ')}'"
+        when TrueClass, FalseClass
+          param_value
         else
           "'#{param_value}'"
         end
